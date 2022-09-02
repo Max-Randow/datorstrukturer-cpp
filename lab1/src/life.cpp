adccd35db022680615471348b219aa06b7061bf5
William <wilmi895@student.liu.se>
2022-09-02 18:27:16 +0200

    Updated project config file. Added Grid initialisation by reading file

--- lab1/src/life.cpp
+++ lab1/src/life.cpp
@@ -1,18 +1,44 @@
-// This is the CPP file you will edit and turn in.
-// Also remove these comments here and add your own.
-// TODO: remove this comment header
-
+#include <array>
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <string>
+
 #include "grid.h"
 #include "lifeutil.h"
 
+using namespace std;
+
+Grid<char> init_game_of_life(string const& file_name) noexcept {
+	ifstream rfile(file_name, ios_base::in);
+
+	const size_t header_size = 4;
+	array<char, header_size> header {};
+	rfile.read(header.data(), header_size);
+	// Convert character to corresponding number.
+	Grid<char> game_of_life(header[0] - '0', header[2] - '0');
+
+	string line_buf;
+	line_buf.reserve(static_cast<size_t>(game_of_life.numRows()));
+	int row = 0;
+
+	while (getline(rfile, line_buf)) {
+		for (int col = 0; col < game_of_life.numCols(); col++) {
+			game_of_life.set(row, col, line_buf[static_cast<size_t>(col)]);
+		}
+		row++;
+	}
+
+	return game_of_life;
+}
+
 int main() {
-  // TODO: Finish the program!
-  Grid<char> grid(5, 5);
+	// ios_base::sync_with_stdio(false);
+	// cin.tie(NULL);
 
-  std::cout << grid.numCols() << '\n';
+	Grid<char> game_of_life = init_game_of_life("simple.txt");
 
-  std::cout << "Have a nice Life! " << std::endl;
-  return 0;
+
+	cout << "Have a nice Life! " << endl;
+	return 0;
 }
