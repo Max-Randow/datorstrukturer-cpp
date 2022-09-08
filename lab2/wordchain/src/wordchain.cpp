556f921b2c7b2d6f2ec87865a4328cd1e01ce212
William <wilmi895@student.liu.se>
2022-09-08 22:05:01 +0200

    Implemented word chain algorithm

--- lab2/wordchain/src/wordchain.cpp
+++ lab2/wordchain/src/wordchain.cpp
@@ -1,85 +1,122 @@
 #include <fstream>
 #include <iostream>
-#include <string>
 #include <queue>
 #include <stack>
-#include<set>
+#include <string>
+#include <unordered_set>
 using namespace std;
 
-const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
+const string ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
-set<string> init_dict(string const& file_name){
+/*
+ * Initialises the english dictionary from a dictionary file.
+ */
+unordered_set<string> init_dictionary(string const& file_name) {
+	unordered_set<string> dictionary;
+	string buffer;
+	ifstream rfile(file_name);
 
-    set<string> dict_set;
-    string buffer;
-    ifstream rfile(file_name);
-
-    // Setup set with given dictionary
-    while(getline(rfile,buffer)){
-        dict_set.insert(buffer);
-    }
-    rfile.close();
-    return dict_set;
+	// Setup set with given dictionary
+	while (getline(rfile, buffer)) {
+		dictionary.insert(buffer);
+	}
+	return dictionary;
 }
 
+/*
+ * Generates all the word neighbours, meaning word who differ by one letter
+ * and exists in the english dictionary.
+ */
+vector<string> word_neighbours(string const& word,
+							   unordered_set<string> const& dictionary) {
+	vector<string> neighbours;
 
-vector<string> word_neighbours(string const& word, set<string> const& dictionary){
-    vector<string> neighbours;
-    for(int i = 0; i < word.size(); i++){
-        string tmp_word = word;
-        for(char const& letter : ALPHABET){
-            tmp_word[i] = letter;
-            // Adds word to the neighbour list if it exists in the dictionary, the word itself is not a neighbour to itself
-            if(dictionary.find(tmp_word) != dictionary.end() && tmp_word != word){
-                neighbours.push_back(tmp_word);
-            }
+	// Change each letter in the word to find neighbour words.
+	// Meaning word with a one letter difference.
+	for (size_t i = 0; i < word.size(); i++) {
+		string tmp_word = word;
 
-        }
-    }
-    return neighbours;
+		for (char const letter : ALPHABET) {
+			tmp_word[i] = letter;
+			// Adds word to the neighbour list if it exists in the dictionary,
+			// the word itself is not a neighbour to itself
+			if (tmp_word != word &&
+				dictionary.find(tmp_word) != dictionary.end()) {
+				neighbours.push_back(tmp_word);
+			}
+		}
+	}
+	return neighbours;
 }
 
-stack<string> word_chain(string const& word1, string const& word2, set<string> const& dictionary){
-    // vars n bars (sick yao)
-    queue<stack<string>> chain_queue;
-    stack<string> first_chain;
-    set<string> used_words;
-    first_chain.push(word1);
-    chain_queue.push(first_chain);
+/*
+ * Finds the shortest word chain between two words.
+ * Uses BFS algorithm to explore neighbour words.
+ */
+stack<string> word_chain(string const& word1,
+						 string const& word2,
+						 unordered_set<string> const& dictionary) {
+	// Initialize used words set with the starting word.
+	unordered_set<string> used_words;
+	used_words.insert(word1);
 
-    while(!chain_queue.empty()){
-        stack<string> part_chain = chain_queue.pop();
-        if(part_chain.top() == word2) {
-            return part_chain;
-        }
-       vector<string> neighbours = word_neighbours(part_chain.top(), dictionary);
-       for(string const& word : neighbours){
-            if(used_words.find(word) != used_words.end()){
-                stack<string> chain_copy = part_chain;
-                chain_copy.push(word);
-                chain_queue.push(chain_copy);
-                used_words.insert(word);
-            }
-       }
-        
+	// Initialize the first word chain with the starting word.
+	stack<string> first_chain;
+	first_chain.push(word1);
 
-    }
+	queue<stack<string>> word_chains;
+	word_chains.push(first_chain);
+
+	// BFS algorithm to explore all word neighbours and find the shortest word
+	// chain.
+	while (!word_chains.empty()) {
+		stack<string> partial_chain = move(word_chains.front());
+		word_chains.pop();
+		string const& last_chain_word = partial_chain.top();
+
+		// If the last word in the chain matches target word, return chain
+		if (last_chain_word == word2) {
+			return partial_chain;
+		}
+
+		// Get all word neighbours to last word in chain.
+		vector<string> const neighbours =
+			word_neighbours(last_chain_word, dictionary);
+
+		// Explore neighbour words.
+		for (string const& neighbor_word : neighbours) {
+			if (used_words.find(neighbor_word) == used_words.end()) {
+				stack<string> chain_copy = partial_chain;
+				chain_copy.push(neighbor_word);
+				word_chains.push(chain_copy);
+				used_words.insert(neighbor_word);
+			}
+		}
+	}
+
+	return {};
 }
 
 int main() {
-    cout << "Welcome to TDDD86 Word Chain." << endl;
-    cout << "If you give me two English words, I will transform the" << endl;
-    cout << "first into the second by changing one letter at a time." << endl;
-    cout << endl;
+	cout << "Welcome to TDDD86 Word Chain.\n";
+	cout << "If you give me two English words, I will transform the\n";
+	cout << "first into the second by changing one letter at a time.\n\n";
 
-    const set<string> word_dict = init_dict("dictionary.txt");
+	const unordered_set<string> word_dict = init_dictionary("dictionary.txt");
 
+	string word1;
+	string word2;
+	cout << "Please type two words: ";
+	cin >> word1 >> word2;
 
+	stack<string> chain = word_chain(word1, word2, word_dict);
 
-    cout << "Please type two words: ";
+	// Print out shortest chain in reverse order.
+	while (!chain.empty()) {
+		cout << chain.top() << ' ';
+		chain.pop();
+	}
+	cout << '\n';
 
-
-    // TODO: Finish the program!
-
-    return 0;
+	return 0;
 }
