4b4300a3bddef7fb73982b5291e6a6dae8a10fd1
William <wilmi895@student.liu.se>
2022-09-20 10:04:01 +0200

    Fixed bug where user inputed word length was not chosen

--- lab2/evilhangman/src/evilhangman.cpp
+++ lab2/evilhangman/src/evilhangman.cpp
@@ -222,12 +222,11 @@ void game_loop(vector<string> const& dictionary,
 		unsigned int correct_letters = 0;
 		unsigned int guesses_left	 = settings.guess_count;
 		string current_word(settings.word_length, '-');
-		vector<string> current_word_family = dictionary;
 
 		// Extract words with provided length.
-		vector<string> equal_len_words;
+		vector<string> current_word_family;
 		copy_if(dictionary.cbegin(), dictionary.cend(),
-				back_inserter(equal_len_words),
+				back_inserter(current_word_family),
 				[&settings](string const& word) noexcept {
 					return word.length() == settings.word_length;
 				});
