577e909283fbdef5a0d7d9bb26d6b191f120304e
William <wilmi895@student.liu.se>
2022-12-13 22:45:28 +0100

    EL1: added comments

--- EL1/src/encoding.cpp
+++ EL1/src/encoding.cpp
@@ -1,8 +1,11 @@
-﻿// This is the CPP file you will edit and turn in.
-// Also remove these comments here and add your own, along with
-// comments on every function and on complex code sections.
-// TODO: remove this comment header
-
+﻿/*
+ * Author: wilmi895
+ * File: encoding.cpp
+ * Usage:
+ * Defines functions for compressing a file using huffman encoding.
+ * Contains several helper functions for constructing the frequencies
+ * and the encoding tree used by the huffman algorithm.
+ */
 #include "encoding.h"
 
 #include <queue>
@@ -26,7 +29,9 @@ map<int, int> buildFrequencyTable(istream& input) {
 	return freqTable;
 }
 
-
+/*
+ * Builds the huffman encoding tree from a character frequency table.
+ */
 HuffmanNode* buildEncodingTree(map<int, int> const& freqTable) {
 	priority_queue<HuffmanNode> heap {};
 
@@ -37,7 +42,6 @@ HuffmanNode* buildEncodingTree(map<int, int> const& freqTable) {
 	// Continually join two nodes in a root node to create the tree.
 	while (heap.size() > 1) {
 		HuffmanNode root {};
-
 		root.zero = new HuffmanNode(heap.top());
 		heap.pop();
 		root.one = new HuffmanNode(heap.top());
@@ -50,6 +54,9 @@ HuffmanNode* buildEncodingTree(map<int, int> const& freqTable) {
 	return new HuffmanNode(heap.top());
 }
 
+/*
+ * Helper function for building the encoding map.
+ */
 void buildEncodingMapHelper(HuffmanNode const* const root,
 							string encoding,
 							map<int, string>& encodingMap) {
@@ -62,6 +69,9 @@ void buildEncodingMapHelper(HuffmanNode const* const root,
 	buildEncodingMapHelper(root->one, encoding + "1", encodingMap);
 }
 
+/*
+ * Builds the huffman encoding map using an encoding tree.
+ */
 map<int, string> buildEncodingMap(HuffmanNode* encodingTree) {
 	map<int, string> encodingMap;
 
@@ -70,15 +80,23 @@ map<int, string> buildEncodingMap(HuffmanNode* encodingTree) {
 	return encodingMap;
 }
 
+/*
+ * Converts a ascii char number to a decimal int.
+ */
 int asciiToDecimal(char const ascii) { return ascii - '0'; }
 
+/*
+ * Takes a string of bits and writes those bits to the output.
+ */
 void writeBits(string const& input, obitstream& output) {
 	for (char const bit : input) {
-		// Convert ascii to decimal
 		output.writeBit(asciiToDecimal(bit));
 	}
 }
 
+/*
+ * Encodes an input stream using huffman encoding.
+ */
 void encodeData(istream& input,
 				map<int, string> const& encodingMap,
 				obitstream& output) {
@@ -92,6 +110,9 @@ void encodeData(istream& input,
 	writeBits(encodingMap.at(PSEUDO_EOF), output);
 }
 
+/*
+ * Decodes a huffman encoded input stream.
+ */
 void decodeData(ibitstream& input, HuffmanNode* encodingTree, ostream& output) {
 	HuffmanNode const* current_node = encodingTree;
 
@@ -114,6 +135,9 @@ void decodeData(ibitstream& input, HuffmanNode* encodingTree, ostream& output) {
 	}
 }
 
+/*
+ * Constructs and writes the header for a huffman encoded file.
+ */
 void writeHeader(obitstream& output, map<int, int> const& frequencyTable) {
 	stringstream header;
 	header << '{';
@@ -128,6 +152,10 @@ void writeHeader(obitstream& output, map<int, int> const& frequencyTable) {
 	output.write(header.str().c_str(), header.tellp());
 }
 
+/*
+ * Compresses input stream using Huffman encoding and places it in output
+ * stream.
+ */
 void compress(istream& input, obitstream& output) {
 	map<int, int> const frequencyTable = buildFrequencyTable(input);
 	HuffmanNode* const encodingTree	   = buildEncodingTree(frequencyTable);
@@ -140,6 +168,9 @@ void compress(istream& input, obitstream& output) {
 	encodeData(input, encodingMap, output);
 }
 
+/*
+ * Reads in header from huffman encoded file.
+ */
 map<int, int> readHeader(ibitstream& input) {
 	map<int, int> frequencyTable {};
 	string character;
@@ -149,17 +180,20 @@ map<int, int> readHeader(ibitstream& input) {
 	while (!input.fail() && byte != '}') {
 		byte = input.get();
 
+		// Read a character from the header.
 		while (!input.fail() && byte != ':') {
 			character += to_string(asciiToDecimal(byte));
 			byte = input.get();
 		}
 
+		// Read a frequency for a character from the header.
 		byte = input.get();
 		while (!input.fail() && byte != ',' && byte != '}') {
 			frequency += to_string(asciiToDecimal(byte));
 			byte = input.get();
 		}
 
+		// Store the character/frequency pair as integers.
 		frequencyTable[stoi(character)] = stoi(frequency);
 		character.clear();
 		frequency.clear();
@@ -168,6 +202,9 @@ map<int, int> readHeader(ibitstream& input) {
 	return frequencyTable;
 }
 
+/*
+ * Decompresses a huffman encoded input stream and places it in output stream.
+ */
 void decompress(ibitstream& input, ostream& output) {
 	map<int, int> frequencyTable	= readHeader(input);
 	HuffmanNode* const encodingTree = buildEncodingTree(frequencyTable);
@@ -175,6 +212,9 @@ void decompress(ibitstream& input, ostream& output) {
 	freeTree(encodingTree);
 }
 
+/*
+ * Frees a huffmantree
+ */
 void freeTree(HuffmanNode* node) {
 	if (node == nullptr) {
 		return;
