31da221733309f55c48ffc99ec2fed17dce858da
William Minidis <wilmi895@student.liu.se>
2022-12-19 11:07:28 +0100

    lab7: removed map where points are stored. Uneccessary since we only need to count points.

--- lab7/src/fast.cpp
+++ lab7/src/fast.cpp
@@ -23,85 +23,82 @@ static const int SCENE_WIDTH  = 512;
 static const int SCENE_HEIGHT = 512;
 
 void render_points(QGraphicsScene* scene, const vector<Point>& points) {
-	for (const auto& point : points) {
-		point.draw(scene);
-	}
+    for (const auto& point : points) {
+        point.draw(scene);
+    }
 }
 
 void render_line(QGraphicsScene* scene, const Point& p1, const Point& p2) {
-	p1.lineTo(scene, p2);
+    p1.lineTo(scene, p2);
 }
 
 int main(int argc, char* argv[]) {
-	QApplication a(argc, argv);
+    QApplication a(argc, argv);
 
-	// open file
-	string filename = "mystery10089.txt";
-	ifstream input;
-	input.open(filename);
+    // open file
+    string filename = "input400.txt";
+    ifstream input;
+    input.open(filename);
 
-	// the vector of points
-	vector<Point> points;
+    // the vector of points
+    vector<Point> points;
 
-	// read points from file
-	unsigned int N = 0;
-	int x		   = 0;
-	int y		   = 0;
+    // read points from file
+    unsigned int N = 0;
+    int x		   = 0;
+    int y		   = 0;
 
-	input >> N;
-	points.reserve(N);
+    input >> N;
+    points.reserve(N);
 
-	for (unsigned i = 0; i < N; ++i) {
-		input >> x >> y;
-		points.emplace_back(x, y);
-	}
-	input.close();
+    for (unsigned i = 0; i < N; ++i) {
+        input >> x >> y;
+        points.emplace_back(x, y);
+    }
+    input.close();
 
-	// setup graphical window
-	auto* view	= new QGraphicsView();
-	auto* scene = new QGraphicsScene(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
-	view->setScene(scene);
-	// draw points to screen all at once
-	render_points(scene, points);
-	view->scale(1, -1);	 // screen y-axis is inverted
-	view->resize(view->sizeHint());
-	view->setWindowTitle("Fast Pattern Recognition");
-	view->show();
+    // setup graphical window
+    auto* view	= new QGraphicsView();
+    auto* scene = new QGraphicsScene(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
+    view->setScene(scene);
+    // draw points to screen all at once
+    render_points(scene, points);
+    view->scale(1, -1);	 // screen y-axis is inverted
+    view->resize(view->sizeHint());
+    view->setWindowTitle("Fast Pattern Recognition");
+    view->show();
 
-	// sort points by natural order
-	// makes finding endpoints of line segments easy
-	sort(points.begin(), points.end());
-	auto begin = chrono::high_resolution_clock::now();
+    // sort points by natural order
+    // makes finding endpoints of line segments easy
+    sort(points.begin(), points.end());
+    auto begin = chrono::high_resolution_clock::now();
 
-	for (unsigned i = 0; i < N - 1; i++) {
-		unordered_map<double, Point> slopes {};
-		unordered_map<double, int> counts {};
-		Point const origin = points[i];
+    for (unsigned i = 0; i < N - 1; i++) {
+        unordered_map<double, int> counts {};
+        Point const origin = points[i];
 
-		for (unsigned j = i + 1; j < N; j++) {
-			Point const next_point = points[j];
-			double const slope	   = origin.slopeTo(next_point);
+        for (unsigned j = i + 1; j < N; j++) {
+            Point const next_point = points[j];
+            double const slope	   = origin.slopeTo(next_point);
 
-			// log(size of counts) complexity
-			int& count = counts[slope];
+            // lookup: log(N)
+            int& count = counts[slope];
 
-			if (count >= 2) {
-				render_line(scene, origin, next_point);
-				QApplication::processEvents();
+            if (count >= 2) {
+                render_line(scene, origin, next_point);
+                QApplication::processEvents();
 
-			} else {
-				// emplace: log(size of slopes)
-				slopes.emplace(slope, next_point);
-				count++;
-			}
-		}
-	}
+            } else {
+                count++;
+            }
+        }
+    }
 
-	auto end = chrono::high_resolution_clock::now();
-	cout
-		<< "Computing line segments took "
-		<< std::chrono::duration_cast<chrono::milliseconds>(end - begin).count()
-		<< " milliseconds." << endl;
+    auto end = chrono::high_resolution_clock::now();
+    cout
+        << "Computing line segments took "
+        << std::chrono::duration_cast<chrono::milliseconds>(end - begin).count()
+        << " milliseconds." << endl;
 
-	return a.exec();  // start Qt event loop
+    return a.exec();  // start Qt event loop
 }
