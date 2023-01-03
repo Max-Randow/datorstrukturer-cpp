ec8d31f17805d74c68416294acd3e17e058e0e11
William Minidis <wilmi895@student.liu.se>
2023-01-03 15:47:32 +0100

    lab8: implemented bfs and dfs. removed separate algorithm files.

--- lab8/src/trailblazer.cpp
+++ lab8/src/trailblazer.cpp
@@ -1,24 +1,18 @@
-// This is the CPP file you will edit and turn in.
-// Also remove these comments here and add your own, along with
-// comments on every function and on complex code sections.
-// TODO: write comment header for this file; remove this comment
+/*
+ * Authors: wilmi895 maxra518
+ * Usage:
+ *
+ */
 
 #include "costs.h"
 #include "trailblazer.h"
-// TODO: include any other headers you need; remove this comment
+#include "BasicGraph.h"
+#include <queue>
+#include <algorithm>
+
 using namespace std;
 
-/*
-vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
-    vector<Vertex*> path;
-    return path;
-}
-
-vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
+vector<Node *> aStar(BasicGraph& graph, Vertex* start, Vertex* end) {
     // TODO: implement this function; remove these comments
     //       (The function body code provided below is just a stub that returns
     //        an empty vector so that the overall project will compile.
@@ -36,12 +30,77 @@ vector<Node *> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end)
     return path;
 }
 
-vector<Node *> aStar(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
+
+bool depthFirstSearchImpl(BasicGraph& graph, Vertex* start, Vertex* end, vector<Vertex*>& path) {
+    start->visited = true;
+    start->setColor(GREEN);
+
+    if(start == end) {
+        return true;
+    }
+
+    for(Vertex* const neighbor : graph.getNeighbors(start)) {
+        if(!neighbor->visited && depthFirstSearchImpl(graph, neighbor, end, path)) {
+            path.push_back(neighbor);
+            return true;
+        }
+    }
+
+    start->setColor(GRAY);
+    return false;
+}
+
+vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
+    graph.resetData();
+
     vector<Vertex*> path;
+
+    depthFirstSearchImpl(graph, start, end, path);
+    path.push_back(start);
+    reverse(path.begin(), path.end());
+
     return path;
 }
-*/
+
+
+using namespace std;
+
+vector<Node*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
+    graph.resetData();
+
+    vector<Vertex*> path;
+    queue<Vertex*> queue;
+    start->visited = true;
+    start->setColor(YELLOW);
+    queue.push(start);
+
+    while(!queue.empty()) {
+        Vertex* current_vertex = queue.front();
+        queue.pop();
+        current_vertex->setColor(GREEN);
+
+        if(current_vertex == end){
+            while(current_vertex != start){
+                path.push_back(current_vertex);
+                current_vertex = current_vertex->previous;
+            }
+
+            path.push_back(start);
+            reverse(path.begin(), path.end());
+            break;
+        }
+
+        for(Vertex* const neighbour : graph.getNeighbors(current_vertex)){
+            if(neighbour->visited){
+                continue;
+            }
+
+            neighbour->visited = true;
+            neighbour->previous = current_vertex;
+            neighbour->setColor(YELLOW);
+            queue.push(neighbour);
+        }
+    }
+
+    return path;
+}
