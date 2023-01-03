ed2a8be6ff0165708ce582d94d79d32fec57d31c
Max Randow <maxra518@student.liu.se>
2023-01-03 14:02:13 +0100

    lab8 BFS

--- lab8/src/bfs.cpp
+++ lab8/src/bfs.cpp
@@ -1,13 +1,48 @@
+#include "BasicGraph.h"
 #include "costs.h"
 #include "trailblazer.h"
-// TODO: include any other headers you need; remove this comment
+#include<queue>
+#include<unordered_set>
+#include<set>
+#include<algorithm>
+
+
 using namespace std;
 
 vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
-    vector<Vertex*> path;
+        vector<Vertex*> path;
+        queue<Vertex*> queue;
+        queue.push(start);
+        start->visited = true;
+        start->setColor(GREEN);
+        while(!queue.empty()){
+
+            Vertex* node = queue.front();
+            queue.pop();
+            if(node == end){
+                while(node != start){
+                    path.push_back(node);
+                    node = node->previous;
+                }
+                path.push_back(start);
+                reverse(path.begin(), path.end());
+                break;
+            } 
+            
+            
+            for(Vertex* neighbour : graph.getNeighbors(node)){
+                if(!neighbour->visited){
+                    queue.push(neighbour);
+                    neighbour->setColor(YELLOW);
+                    neighbour->visited = true;
+                    neighbour->setColor(GREEN);
+                    neighbour->previous = node;
+                }
+            }
+
+        }
     return path;
 }
+
+
+
